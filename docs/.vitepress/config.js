a16362c4bc030b49d996a3b48ddd31b52e5bcb58
zhuangjinan <zhuangjinan2018@gmail.com>
2022-06-17 17:24:56 +0800

    add notes

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,20 +1,22 @@
 import { defineConfig } from "vitepress";
 
 export default defineConfig({
-  title: "爱叫啥叫啥",
+  title: "Zhuang Jinan",
+  titleTemplate: "Zhuang Jinan",
   lang: "zh-cmn-Hans",
   description: "Just playing around.",
   themeConfig: {
     nav: [
       { text: "Blog", link: "/blog/" },
-      { text: "Notes", link: "/notes" },
+      { text: "Notes", link: "/notes/" },
     ],
-    logo: "/badminton.svg",
+    logo: "/21.svg",
     socialLinks: [{ icon: "github", link: "https://github.com/FuZhouJohn" }],
     sidebar: {},
     footer: {
       message: "Released under the MIT License.",
-      copyright: "Copyright © 2022-present Zhuang Ji Nan",
+      copyright: "Copyright © 2022-present Zhuang Jinan",
     },
   },
+  lastUpdated: true,
 });
---
+++ docs/notes/index.md
@@ -0,0 +1,29 @@
+---
+layout: doc
+title: Notes
+---
+
+# Notes
+
+## 查找单链表中间点
+
+_2022/06/17_
+
+假设两「人」同时链表起点出发，A 一次走一步，B 一次走两步，当 B 到达终点时，A 所在的点为中心点。 ​​​​
+
+```js
+const middleNode = function (head) {
+  let slow = head,
+    fast = head;
+  while (fast && fast.next) (slow = slow.next), (fast = fast.next.next);
+  return slow;
+};
+```
+
+## 锁定 npm 包安装时的版本号
+
+_2022/05/27_
+
+```bash
+npm config set save-prefix=''
+```
