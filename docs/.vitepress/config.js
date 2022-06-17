5cc819155c392a2b9e11f06bd8d128183ae2f005
zhuangjinan <zhuangjinan2018@gmail.com>
2022-06-17 15:24:38 +0800

    test

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,20 @@
-export default {
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
   title: "爱叫啥叫啥",
+  lang: "zh-cmn-Hans",
   description: "Just playing around.",
-};
+  themeConfig: {
+    nav: [
+      { text: "Blog", link: "/blog/" },
+      { text: "Notes", link: "/notes" },
+    ],
+    logo: "/badminton.svg",
+    socialLinks: [{ icon: "github", link: "https://github.com/FuZhouJohn" }],
+    sidebar: {},
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2022-present Zhuang Ji Nan",
+    },
+  },
+});
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,12 @@
-# Hello VitePress
+---
+layout: home
+
+hero:
+  name: VuePress
+  text: Vite & Vue powered static site generator.
+  tagline: Lorem ipsum...
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /blog/
+---
